e2cf307ef461b6ad9c63d2db13478e8a049ef34d
ꜱᴀɴᴅᴇᴇᴘ ꜱʜᴀʀᴍᴀ <sandeepshrmaxcoder@gmail.com>
2024-12-02 14:11:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+API_ID = ''          # Get from https://my.telegram.org/auth
+API_HASH = ''        # Get from https://my.telegram.org/auth
+BOT_TOKEN = ''      # If using a bot token
+MONGO_URI = 'mongodb://localhost:27017'  # MongoDB URI, default is localhost
